6064aa48ee053b09aece9129aaa14c6996ad1c0c
tycho garen <garen@tychoish.com>
2022-05-27 07:29:37 -0400

    model: seralize user type

--- model/user.go
+++ model/user.go
@@ -1,6 +1,9 @@
 package model
 
 import (
+	"encoding/json"
+	"errors"
+
 	"github.com/tendermint/tendermint/crypto"
 	"github.com/tendermint/tendermint/internal/jsontypes"
 )
@@ -9,7 +12,50 @@ type User struct {
 	Name      string
 	PubKey    crypto.PubKey
 	Moderator bool
+	Banned    bool
+	// schema versioning
+	Version int
 }
 
-func (u *User) TypeTag() string { return "io.interchain.forum" }
-func init()                     { jsontypes.MustRegister(&User{}) }
+func (u *User) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		Name      string `json:"name"`
+		PubKey    []byte `json:"pub_key"`
+		Moderator bool   `json:"moderator"`
+		Banned    bool   `json:"banned"`
+		Version   int    `json:"version"`
+	}{
+		Name:      u.Name,
+		PubKey:    jsontypes.Marshal(u.PubKey),
+		Moderator: u.Moderator,
+		Version:   u.Version,
+	})
+}
+
+func (u *User) UnmarshalJSON(in []byte) error {
+	intermediate := struct {
+		Name      string `json:"name"`
+		PubKey    []byte `json:"pub_key"`
+		Moderator bool   `json:"moderator"`
+		Banned    bool   `json:"banned"`
+		Version   int    `json:"version"`
+	}{}
+
+	if err := json.Unmarshal(in, &intermediate); err != nil {
+		return err
+	}
+
+	if u.Version != intermediate.Version {
+		return errors.New("schema has changed")
+	}
+
+	if err := jsontypes.Unmarshal(intermediate.PubKey, u.PubKey); err != nil {
+		return err
+	}
+
+	u.Name = intermediate.Name
+	u.Moderator = intermediate.Moderator
+	u.Banned = intermediate.Banned
+
+	return nil
+}
