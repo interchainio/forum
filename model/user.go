9ffa4bd3a5a58c21ad77e10dc0a9d97334f98f70
tycho garen <garen@tychoish.com>
2022-05-27 07:32:33 -0400

    user: use easily marshallable types

--- model/user.go
+++ model/user.go
@@ -1,61 +1,14 @@
 package model
 
 import (
-	"encoding/json"
-	"errors"
-
-	"github.com/tendermint/tendermint/crypto"
-	"github.com/tendermint/tendermint/internal/jsontypes"
+	"github.com/tendermint/tendermint/crypto/ed25519"
 )
 
 type User struct {
 	Name      string
-	PubKey    crypto.PubKey
+	PubKey    ed25519.PubKey // this is just a wrapper around bytes
 	Moderator bool
 	Banned    bool
 	// schema versioning
 	Version int
-}
-
-func (u *User) MarshalJSON() ([]byte, error) {
-	return json.Marshal(struct {
-		Name      string `json:"name"`
-		PubKey    []byte `json:"pub_key"`
-		Moderator bool   `json:"moderator"`
-		Banned    bool   `json:"banned"`
-		Version   int    `json:"version"`
-	}{
-		Name:      u.Name,
-		PubKey:    jsontypes.Marshal(u.PubKey),
-		Moderator: u.Moderator,
-		Version:   u.Version,
-	})
-}
-
-func (u *User) UnmarshalJSON(in []byte) error {
-	intermediate := struct {
-		Name      string `json:"name"`
-		PubKey    []byte `json:"pub_key"`
-		Moderator bool   `json:"moderator"`
-		Banned    bool   `json:"banned"`
-		Version   int    `json:"version"`
-	}{}
-
-	if err := json.Unmarshal(in, &intermediate); err != nil {
-		return err
-	}
-
-	if u.Version != intermediate.Version {
-		return errors.New("schema has changed")
-	}
-
-	if err := jsontypes.Unmarshal(intermediate.PubKey, u.PubKey); err != nil {
-		return err
-	}
-
-	u.Name = intermediate.Name
-	u.Moderator = intermediate.Moderator
-	u.Banned = intermediate.Banned
-
-	return nil
 }
