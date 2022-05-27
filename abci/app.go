244a9687b62b80083235621bfce7e870a130958f
William Banfield <wbanfield@gmail.com>
2022-05-27 07:17:19 -0400

    add scaffolding for abci application

---
+++ abci/app.go
@@ -0,0 +1,79 @@
+package abci
+
+import (
+	"context"
+
+	abci "github.com/tendermint/tendermint/abci/types"
+)
+
+var _ abci.Application = App{}
+
+type App struct{}
+
+// Info/Query Connection
+// Return application info
+func (App) Info(_ context.Context, _ *abci.RequestInfo) (*abci.ResponseInfo, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (App) Query(_ context.Context, _ *abci.RequestQuery) (*abci.ResponseQuery, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+// Mempool Connection
+// Validate a tx for the mempool
+func (App) CheckTx(_ context.Context, _ *abci.RequestCheckTx) (*abci.ResponseCheckTx, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+// Consensus Connection
+// Initialize blockchain w validators/other info from TendermintCore
+func (App) InitChain(_ context.Context, _ *abci.RequestInitChain) (*abci.ResponseInitChain, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (App) PrepareProposal(_ context.Context, _ *abci.RequestPrepareProposal) (*abci.ResponsePrepareProposal, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (App) ProcessProposal(_ context.Context, _ *abci.RequestProcessProposal) (*abci.ResponseProcessProposal, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+// Commit the state and return the application Merkle root hash
+func (App) Commit(_ context.Context) (*abci.ResponseCommit, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+// Create application specific vote extension
+func (App) ExtendVote(_ context.Context, _ *abci.RequestExtendVote) (*abci.ResponseExtendVote, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+// Verify application's vote extension data
+func (App) VerifyVoteExtension(_ context.Context, _ *abci.RequestVerifyVoteExtension) (*abci.ResponseVerifyVoteExtension, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+// Deliver the decided block with its txs to the Application
+func (App) FinalizeBlock(_ context.Context, _ *abci.RequestFinalizeBlock) (*abci.ResponseFinalizeBlock, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+// State Sync Connection
+// List available snapshots
+func (App) ListSnapshots(_ context.Context, _ *abci.RequestListSnapshots) (*abci.ResponseListSnapshots, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (App) OfferSnapshot(_ context.Context, _ *abci.RequestOfferSnapshot) (*abci.ResponseOfferSnapshot, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (App) LoadSnapshotChunk(_ context.Context, _ *abci.RequestLoadSnapshotChunk) (*abci.ResponseLoadSnapshotChunk, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (App) ApplySnapshotChunk(_ context.Context, _ *abci.RequestApplySnapshotChunk) (*abci.ResponseApplySnapshotChunk, error) {
+	panic("not implemented") // TODO: Implement
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,18 @@ module github.com/interchainio/forum
 
 go 1.18
 
-require github.com/tendermint/tendermint v0.36.0-dev.0.20220527091719-9027401de465 // indirect
+require github.com/tendermint/tendermint v0.36.0-dev.0.20220527091719-9027401de465
+
+require (
+	github.com/btcsuite/btcd v0.22.1 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/oasisprotocol/curve25519-voi v0.0.0-20210609091139-0a56a4bca00b // indirect
+	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
+	golang.org/x/net v0.0.0-20220412020605-290c469a71a5 // indirect
+	golang.org/x/sys v0.0.0-20220422013727-9388b58f7150 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20220414192740-2d67ff6cf2b4 // indirect
+	google.golang.org/grpc v1.46.2 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+)
