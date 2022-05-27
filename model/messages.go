2a550ad217e5d7fe13ffd792a49eccf2e34c03cb
tycho garen <garen@tychoish.com>
2022-05-27 07:49:19 -0400

    messages: add fields

--- model/messages.go
+++ model/messages.go
@@ -1,6 +1,18 @@
 package model
 
-type MsgSendMessage struct{}
-type MsgBan struct{}
-type MsgAddModerator struct{}
-type MsgRemoveModerator struct{}
+import "github.com/tendermint/tendermint/crypto/ed25519"
+
+type MsgSendMessage struct {
+	Text string
+	From ed25519.PubKey
+}
+
+type MsgSetBan struct {
+	User  ed25519.PubKey
+	State bool
+}
+
+type MsgSetModerator struct {
+	User  ed25519.PubKey
+	State bool
+}
